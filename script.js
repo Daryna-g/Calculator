8b8a006512253ba75b05354223832db5624247b3
Daryna-g <gickad@gmail.com>
2025-08-10 14:40:17 +0300

    Start write js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const clearBtn = document.querySelector(".clear-btn");
+	const equalBtn = document.querySelector(".equal-btn");
+	const desimalBtn = document.querySelector(".desimal-btn");
+
+	let numbersBtn = document.querySelectorAll(".number-btn");
+})
